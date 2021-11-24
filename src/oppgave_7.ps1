3bee4e35c4e30f09cb2fc5c92f2e20ffbea300a8
BeratUtvikler <RedzepiBerati@gmail.com>
2021-11-24 13:01:34 +0100

    Oppgave7

--- src/oppgave_7.ps1
+++ src/oppgave_7.ps1
@@ -35,14 +35,15 @@ function sumPoengKortstokk {
 
     $poengKortstokk = 0
 
-    foreach ($kort in $kortstokk) {
+    foreach ($kort in $kortstokk) 
+}
         # Undersøk hva en Switch er
         $poengKortstokk += switch ($kort.value) {
             { $_ -cin @('J', 'Q', 'K') } { 10 }
             'A' { 11 }
             default { $kort.value }
         }
-    }
+    
     return $poengKortstokk
     
 
@@ -53,10 +54,46 @@ $meg = $kortstokk[0..1]
     
     $kortstokk = $kortstokk[2..$kortstokk.Count]
 
-    $magnus = $kortstokk[<0..1]
+    $magnus = $kortstokk[0..1]
 
     $kortstokk = $kortstokk[2..$kortstokk.Count]
-
-    Write-Output"meg: $(kortstokkTilStreng -kortstokk $meg)"
     
 
+    Write-Output"meg: $(kortstokkTilStreng -kortstokk $meg)"
+    Write-Output"Magnus: $(kortstokkTilStreng -kortstokk $Magnus)"
+    Write-Output"kortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
+
+
+
+# Resultat vinner
+
+function skrivUtResultat {
+    param (
+        [string]
+        $vinner,        
+        [object[]]
+        $kortStokkMagnus,
+        [object[]]
+        $kortStokkMeg        
+    )
+    Write-Output "Vinner: $vinner"
+    Write-Output "magnus | $(sumPoengKortstokk -kortstokk $kortstokkmagnus) | $(kortstokkTilStreng -kortstokk
+    $kortstokkmagnus)"  
+    Write-Output "meg    | $(sumPoengKortstokk -kortstokk $kortStokkMeg) | $(kortstokkTilStreng -kortstokk
+    $kortStokkMeg)"
+}
+
+# bruker 'blackjack' som et begrep - er 21
+$blackjack = 21
+
+if ((sumPoengKortstokk -kortstokk $meg) -eq $blackjack) {
+    skrivUtResultat -vinner "meg" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+elseif ((sumPoengKortstokk -kortstokk $magnus) -eq $blackjack) {
+    skrivUtResultat -vinner "magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+# Hva er om begge har blackjack? Kanskje det kalles draw?
+# frivillig - kan du endre koden til å ta hensyn til draw?
